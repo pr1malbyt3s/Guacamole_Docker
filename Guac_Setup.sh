a793fb1c5a7165a75e5c836b71db782224c93ef3
pr1malbyt3s <al33w6@gmail.com>
2018-08-03 08:41:11 -0400

    Update Guac_Setup.sh

--- Guac_Setup.sh
+++ Guac_Setup.sh
@@ -4,6 +4,25 @@
 
 #!/bin/bash
 
+#Install docker:
+echo "Installing Docker"
+apt-get update
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+systemctl enable docker
+
+#Pull docker images:
+echo "Pulling Docker images needed."
+docker pull guacamole/guacd
+docker pull guacamole/guacamole
+docker pull postgres
+
+#Start Guacamole installation.
 echo "Starting Guacamole installation."
 #Stop any containers that are running under the same name as ones that will be initialized.
 echo "Stopping any redundant containers."
